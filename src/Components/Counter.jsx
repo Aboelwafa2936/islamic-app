9d1ffd067473cf3d426aac1c905bea0d03b8ddf6
Aboelwafa2936 <aboelwafaa213@gmail.com>
2025-06-24 14:29:19 +0300

    another test

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
 
 
 export default function Counter() {
+    const [counter , setCounter] = useState(0);
+
+
+    return(
+        <>
+        <button onClick={() =>{
+            setCounter(counter + 1)
+        }}>{counter}</button>
+        </>
+    )
+
+
 }
